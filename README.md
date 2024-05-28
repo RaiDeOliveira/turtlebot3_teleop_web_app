06b7a3bdb94ce2bbe2b61ca7a59e30a7f06270b5
RaiDeOLiveira <rai.caje@sou.inteli.edu.br>
2024-05-28 15:35:51 -0300

    docs: add code comments & update README.md

--- README.md
+++ README.md
@@ -1,2 +1,60 @@
-# turtlebot3_teleop_web_app
-Aplicação web simples para teleoperação do Turtlebot 3 via ROS.
+# CLI de controle do Turtlebot3 Burger
+
+Esse repositório armazena um pacote ROS2 com um script de CLI para controle da movimentação do Turtlebot3 Burger. A CLI permite que o usuário controle a movimentação do robô por meio das teclas de setas de seu teclado, bem como que o usuário visualize a velocidade do robô em cada eixo em tempo real.
+
+## Funcionamento
+
+O script responsável pela execução dessa CLI é baseado em Python e usa biblioteca `rclpy` para criar um nó `turtlebot3_teleop` com:
+
+- um **pubblisher**, que publica dados de velocidade para o tópico `cmd_vel` para controlar a velocidade do Turtlebot3 Burger em cada eixo e, portanto, sua movimentação;
+- um **subscriber**, que se subscreve ao tópico `cmd_vel` para receber os dados de velocidade atual do robô e exibí-los ao usuário por meio do terminal.
+
+## Inicialização
+
+### Pré requisitos
+
+1. Git instalado e com chave SSH configurada
+2. Sistema operacional Linux (Ubuntu 22.04 recomendado)
+3. ROS2 instalado
+4. Python instalado
+5. Robô Turtlebot3 Burger conectado na mesma rede wi-fi que seu computador (ou simulador Webots)
+
+### Passo a passo
+
+1. Com seu robô Turtlebot3 Burger com ROS2 e pacotes do Turtlebot3 instalados, rode o seguinte comando numa janela de terminal aberta nele (localmente ou via SSH):
+
+```bash
+ros2 launch turtlebot3_bringup robot.launch.py
+```
+
+> :bulb: **Com simulador Webots:** em uma janela de terminal em seu próprio computador, execute o seguinte comando para executar o Webots com um projeto pré-existente contendo o Turtlebot3 Burger: `ros2 launch webots_ros2_turtlebot robot_launch.py`.
+
+2. Em seu computador, abra uma janela de terminal e clone o repositório com o seguinte comando:
+
+```bash
+ros2 launch turtlebot3_bringup robot.launch.py
+```
+
+3. Adentre o diretório do workspace que contém o pacote ROS2 responsável por inicializar a CLI de controle da movimentação do robô:
+
+```bash
+cd /turtlebot3_teleop_web_app/main_workspace
+```
+
+4. Execute o seguinte comando para compilar o pacote ROS2:
+
+```bash
+colcon build
+```
+
+5. Execute o seguinte comando para configurar seu terminal com as variáveis necessárias para executar o script da CLI:
+
+```bash
+source install/local_setup.bash
+```
+
+6. Execute o seguinte comando para inicializar a CLI:
+
+```bash
+ros2 run turtlebot_movement movement_cli
+```a
--- main_workspace/src/turtlebot_movement/turtlebot_movement/movimentation.py
+++ main_workspace/src/turtlebot_movement/turtlebot_movement/movimentation.py
@@ -1,18 +1,21 @@
-import rclpy
-from rclpy.node import Node
-from geometry_msgs.msg import Twist
-import threading
-import sys, select, os
-import tty, termios, time
+import rclpy  # Importa a biblioteca ROS 2 para Python
+from rclpy.node import Node  # Importa a classe Node da biblioteca ROS 2
+from geometry_msgs.msg import Twist  # Importa a mensagem Twist para comandos de velocidade
+import threading  # Importa a biblioteca de threads
+import sys, select, os  # Importa bibliotecas do sistema
+import tty, termios, time  # Importa bibliotecas para manipulação do terminal e tempo
 
+# Verifica se o sistema operacional é Windows e importa msvcrt se for o caso
 if os.name == 'nt':
     import msvcrt
 
+# Definição de constantes para velocidades máximas e incrementos de velocidade
 BURGER_MAX_LIN_VEL = 0.20
 BURGER_MAX_ANG_VEL = 2.5
 LIN_VEL_STEP_SIZE = 0.1
 ANG_VEL_STEP_SIZE = 0.5
 
+# Mensagem de instruções para controle do robô
 msg = """
 Controle da movimentação do robô
 ---------------------------
@@ -28,32 +31,35 @@ Controles:
 Q : botão de emergência (interromper o programa)
 """
 
+# Define a classe Teleop que herda da classe Node
 class Teleop(Node):
     def __init__(self):
-        super().__init__('turtlebot3_teleop')
-        self.publisher_ = self.create_publisher(Twist, 'cmd_vel', 10)
-        self.subscription = self.create_subscription(Twist, 'cmd_vel', self.listener_callback, 10)
-        self.key_pressed = None
-        self.last_key_pressed = None
-        self.running = True  # To control thread lifecycle
-        self.lock = threading.Lock()
-        self.mensagem = True
-        self.current_twist = Twist()
+        super().__init__('turtlebot3_teleop')  # Inicializa o nó com o nome 'turtlebot3_teleop'
+        self.publisher_ = self.create_publisher(Twist, 'cmd_vel', 10)  # Cria um publisher para o tópico 'cmd_vel'
+        self.subscription = self.create_subscription(Twist, 'cmd_vel', self.listener_callback, 10)  # Cria um subscriber para o tópico 'cmd_vel'
+        self.key_pressed = None  # Variável para armazenar a tecla pressionada
+        self.last_key_pressed = None  # Variável para armazenar a última tecla pressionada
+        self.running = True  # Variável para controlar o ciclo de vida da thread
+        self.lock = threading.Lock()  # Cria um lock para sincronização de threads
+        self.mensagem = True  # Variável para controle de mensagens
+        self.current_twist = Twist()  # Inicializa a mensagem Twist
 
+    # Callback para o subscriber que atualiza a mensagem Twist atual
     def listener_callback(self, msg):
         self.get_logger().info(f'I heard: linear_x={msg.linear.x}, angular_z={msg.angular.z}')
         self.current_twist = msg
 
+    # Função para capturar a tecla pressionada
     def key_poll(self):
         old_attr = termios.tcgetattr(sys.stdin)
         tty.setcbreak(sys.stdin.fileno())
         try:
-            while self.running:  # Check if still supposed to run
+            while self.running:  # Verifica se ainda deve continuar rodando
                 if select.select([sys.stdin], [], [], 0.1)[0]:
                     with self.lock:
                         key = sys.stdin.read(1)
-                        if key == '\x1b':  # Esc key
-                            key += sys.stdin.read(2)  # Read the next two bytes
+                        if key == '\x1b':  # Tecla Esc
+                            key += sys.stdin.read(2)  # Lê os próximos dois bytes
                         self.key_pressed = key
                 else:
                     with self.lock:
@@ -61,89 +67,95 @@ class Teleop(Node):
         finally:
             termios.tcsetattr(sys.stdin, termios.TCSADRAIN, old_attr)
 
+    # Função para imprimir as velocidades atuais a cada 1 segundo
     def print_velocities(self):
         while self.running:
             with self.lock:
                 print(f'Velocidades atuais: linear_x={self.current_twist.linear.x}, angular_z={self.current_twist.angular.z}')
             time.sleep(1)
 
+    # Função principal de controle do robô
     def run(self):
-        target_linear_vel = 0.0
-        target_angular_vel = 0.0
-        key_thread = threading.Thread(target=self.key_poll)
-        print_thread = threading.Thread(target=self.print_velocities)
+        target_linear_vel = 0.0  # Velocidade linear alvo inicial
+        target_angular_vel = 0.0  # Velocidade angular alvo inicial
+        key_thread = threading.Thread(target=self.key_poll)  # Cria a thread para captura de teclas
+        print_thread = threading.Thread(target=self.print_velocities)  # Cria a thread para imprimir velocidades
         key_thread.start()
         print_thread.start()
 
         try:
             print(msg)
-            while rclpy.ok():
+            while rclpy.ok():  # Loop principal enquanto ROS está ok
                 with self.lock:
                     key = self.key_pressed
                     last_key = self.last_key_pressed
 
-                if key == 'q':
+                if key == 'q':  # Botão de emergência
                     print("INTERROMPENDO MOVIMENTAÇÃO DO ROBÔ")
-                    self.running = False  # Signal thread to stop
-                    break  # Exit the loop to stop the node
+                    self.running = False  # Sinaliza para parar as threads
+                    break  # Sai do loop principal
 
-                # Reset speeds if necessary when switching keys
+                # Reseta as velocidades se necessário ao trocar de tecla
                 if key != last_key:
-                    if key in ['\x1b[A', '\x1b[B', None]:  # Arrow up, Arrow down
+                    if key in ['\x1b[A', '\x1b[B', None]:  # Seta para cima, Seta para baixo
                         target_angular_vel = 0.0
-                    if key in ['\x1b[C', '\x1b[D', '\x1b[B', None]:  # Arrow right, Arrow left, Arrow down
+                    if key in ['\x1b[C', '\x1b[D', '\x1b[B', None]:  # Seta para direita, Seta para esquerda, Seta para baixo
                         target_linear_vel = 0.0
 
-                if key == '\x1b[A':  # Arrow up
+                # Ajusta as velocidades com base na tecla pressionada
+                if key == '\x1b[A':  # Seta para cima
                     if self.mensagem:
                         print("O robô está andando para frente")
                     self.mensagem = False
                     target_linear_vel = min(target_linear_vel + LIN_VEL_STEP_SIZE, BURGER_MAX_LIN_VEL)
                 
-                elif key == '\x1b[D':  # Arrow left
+                elif key == '\x1b[D':  # Seta para esquerda
                     if self.mensagem:
                         print("O robô está virando para esquerda")
                     self.mensagem = False
                     target_angular_vel = min(target_angular_vel + ANG_VEL_STEP_SIZE, BURGER_MAX_ANG_VEL)
               
-                elif key == '\x1b[C':  # Arrow right
+                elif key == '\x1b[C':  # Seta para direita
                     if self.mensagem:
                         print("O robô está virando para direita")
                     self.mensagem = False
                     target_angular_vel = max(target_angular_vel - ANG_VEL_STEP_SIZE, -BURGER_MAX_ANG_VEL)
                 
-                elif key == '\x1b[B' or key is None:  # Arrow down
+                elif key == '\x1b[B' or key is None:  # Seta para baixo
                     if not self.mensagem:
                         print("O robô está parando")
                     self.mensagem = True
                     target_linear_vel = 0.0
                     target_angular_vel = 0.0
 
-                self.last_key_pressed = key  # Update the last pressed key
+                self.last_key_pressed = key  # Atualiza a última tecla pressionada
 
+                # Cria a mensagem Twist com as velocidades calculadas
                 twist = Twist()
                 twist.linear.x = float(target_linear_vel)
                 twist.angular.z = float(target_angular_vel)
-                self.publisher_.publish(twist)
+                self.publisher_.publish(twist)  # Publica a mensagem Twist
                 time.sleep(0.1)
 
         except KeyboardInterrupt:
             pass
         
         finally:
+            # Para o robô ao finalizar
             twist = Twist()
             twist.linear.x = 0.0
             twist.angular.z = 0.0
             self.publisher_.publish(twist)
             self.running = False
-            key_thread.join()
-            print_thread.join()
+            key_thread.join()  # Espera a thread de captura de teclas finalizar
+            print_thread.join()  # Espera a thread de impressão de velocidades finalizar
 
+# Função principal para inicializar e rodar o nó
 def main(args=None):
-    rclpy.init(args=args)
-    teleop = Teleop()
-    teleop.run()
-    rclpy.shutdown()
+    rclpy.init(args=args)  # Inicializa o sistema ROS
+    teleop = Teleop()  # Cria uma instância da classe Teleop
+    teleop.run()  # Executa a função run
+    rclpy.shutdown()  # Encerra o sistema ROS
 
 if __name__ == '__main__':
-    main()
+    main()  # Executa a função principal se o script for executado diretamente
