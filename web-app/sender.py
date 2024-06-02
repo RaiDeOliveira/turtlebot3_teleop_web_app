063f068419ba8a42e82bb9df56d9ad1bd2b51e92
RaiDeOLiveira <rai.caje@sou.inteli.edu.br>
2024-06-01 23:41:08 -0300

    feat: latency & styles

--- web-app/sender.py
+++ web-app/sender.py
@@ -1,14 +1,28 @@
 import rclpy
 from rclpy.node import Node
 from sensor_msgs.msg import CompressedImage
+from std_msgs.msg import String
 import cv2
+from time import time
 
 class WebcamPublisher(Node):
     def __init__(self):
         super().__init__('webcam_publisher')
-        self.publisher_ = self.create_publisher(CompressedImage, '/video_frames', 10)
+        self.img_publisher_ = self.create_publisher(
+            CompressedImage,
+            '/video_frames',
+            10
+        )
+        
+        self.latency_publisher = self.create_publisher(
+            String,
+            '/video_latency',
+            10
+        )
+
         self.timer = self.create_timer(0.01, self.timer_callback)  # Publish every 0.1 seconds (10 Hz)
         self.cap = cv2.VideoCapture(0)
+        self.latency = time()
 
     def timer_callback(self):
         ret, frame = self.cap.read()
@@ -18,7 +32,13 @@ class WebcamPublisher(Node):
             msg = CompressedImage()
             msg.format = "jpeg"
             msg.data = buffer.tobytes()
-            self.publisher_.publish(msg)
+            self.img_publisher_.publish(msg)
+        latency_string = str(((int(round(time() - self.latency, 4)*1000))))
+        latency_msg = String()
+        latency_msg.data = latency_string
+        self.latency_publisher.publish(latency_msg)
+        self.latency = time()
+    
 
 def main(args=None):
     rclpy.init(args=args)
