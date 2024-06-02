f8e96e5eb628906999ba3d7156ad29aacf35a18b
RaiDeOLiveira <rai.caje@sou.inteli.edu.br>
2024-06-01 21:19:05 -0300

    feat: video stream working

--- web-app/app.py
+++ web-app/app.py
@@ -1,6 +1,11 @@
 from flask import Flask, render_template
+
+# Flask definitions
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return render_template('index.html')
+
+def main():
+    app.run('0.0.0.0', 8000, True)
